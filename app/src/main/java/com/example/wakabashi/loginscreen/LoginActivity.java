8b2c8b55fd7d1f3fb1b2c4560942371d59312017
wkba <wkba61@gmail.com>
2017-06-28 22:23:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/wakabashi/loginscreen/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.wakabashi.loginscreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wakabashi on 2017/06/27.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
